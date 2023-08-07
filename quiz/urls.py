bbc45fe2312590ba1e4e297ff7e7d6f6d51321b9
IDKOS1 <kss3736@naver.com>
2023-08-07 11:11:17 +0900

    quiz flutter backend

---
+++ quiz/urls.py
@@ -0,0 +1,9 @@
+# 퀴즈 파일에대한 url 관리
+
+from django.urls import path, include
+from .views import helloAPI, randomQuiz
+
+urlpatterns = [
+    path("hello/", helloAPI),
+    path("<int:id>/", randomQuiz)
+]
